6faad3a23e9ffdab3f2c1960517866d4a063b37f
coreymwamba <contact.me@coreymwamba.co.uk>
2018-09-01 16:01:18 +0100

    install LICENSE

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 source ./memo2html.cfg
 sudo install -p -D memo2html -m 0755 $PREFIX/bin/memo2html
 sudo install -p -D memo2html.xsl -m 0644 $PREFIX/share/memo2html.xsl
+sudo install -p -D LICENSE -m 0644 /usr/share/licenses/memo2html/LICENSE
 install -p -D memo2html.cfg -m 0644 $HOME/.config/memo2html.cfg
